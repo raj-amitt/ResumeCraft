ab1702a2c915ad73f591b14fe54b64476ccc7047
Amit Raj <amitraj4381@gmail.com>
2026-09-11 02:07:21 +0530

    fix in the mongoDB connection so connection can b established before sending any req

--- resumeCraft-backend/src/config/database.js
+++ resumeCraft-backend/src/config/database.js
@@ -5,11 +5,16 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 async function connectToDB() {
   try {
+    console.log("Attempting MongoDB connection...");
+    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("Connected to DB");
   } catch (error) {
-    console.log(error);
-    throw error
+    console.error("MongoDB connection failed:");
+    console.error(error);
+    throw error;
   }
 }
 module.exports = connectToDB;
