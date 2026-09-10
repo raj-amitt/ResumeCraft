8435c8c0a439101ba334a3bdd926f05633e4f890
Amit Raj <amitraj4381@gmail.com>
2026-09-11 02:02:01 +0530

    fix in the mongoDB connection so connection can b established before sending any req

--- resumeCraft-backend/server.js
+++ resumeCraft-backend/server.js
@@ -3,8 +3,11 @@ const app = require("../resumeCraft-backend/src/app")
 const connectToDB = require("../resumeCraft-backend/src/config/database")
 
 
-connectToDB();
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Server started on PORT ${process.env.PORT}`)
-})
+connectToDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started on PORT ${process.env.PORT}`);
+  });
+}).catch((error) => {
+  console.error("Database connection failed:", error);
+  process.exit(1);
+});
