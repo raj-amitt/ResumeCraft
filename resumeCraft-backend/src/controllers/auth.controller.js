cf263c65783555008581271c05229bb15562ad13
Amit Raj <amitraj4381@gmail.com>
2026-09-11 11:54:02 +0530

    new db.js change

--- resumeCraft-backend/src/controllers/auth.controller.js
+++ resumeCraft-backend/src/controllers/auth.controller.js
@@ -44,7 +44,11 @@ async function registerUserController(req, res) {
     process.env.JWT_SECRET,
     { expiresIn: "1d" },
   );
-  res.cookie("token", token);
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
   res.status(201).json({
     message: "User registered Successfully",
     user: {
@@ -83,7 +87,11 @@ async function loginUserController(req, res) {
     process.env.JWT_SECRET,
     { expiresIn: "1d" },
   );
-  res.cookie("token", token);
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
   res.status(200).json({
     message: "User logged in Successfully",
     user: {
