6f1a6b70f1440bb29adb0e486e22046d149b9601
Amit Raj <amitraj4381@gmail.com>
2026-09-12 00:30:49 +0530

    register user controller fix

--- resumeCraft-frontend/src/features/auth/pages/Register.jsx
+++ resumeCraft-frontend/src/features/auth/pages/Register.jsx
@@ -64,7 +64,7 @@ const Register = () => {
               name="password"
               placeholder="*******"
               onChange={(e) => {
-                setPassword(e.target).value;
+                setPassword(e.target.value);
               }}
             />
           </div>
