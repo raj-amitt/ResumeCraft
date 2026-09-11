ace091af39ba02d78fe6ed36ae8f172d23fb945c
Amit Raj <amitraj4381@gmail.com>
2026-09-11 11:40:01 +0530

    new db.js change

--- resumeCraft-backend/src/config/database.js
+++ resumeCraft-backend/src/config/database.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-const dns = require("dns");
 
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 async function connectToDB() {
   console.log("========== DB CONNECTION START ==========");
